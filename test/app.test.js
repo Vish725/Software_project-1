99cc66bc6e4147b57c01b78bb07f40c9c17888fd
Vish725 <81736169+Vish725@users.noreply.github.com>
2021-06-11 14:30:52 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -3,85 +3,134 @@ const  x = require("../app");
 const request = require("supertest");
 
 
-
 describe("GET / ", () =>{
-    test("It should respond with facultydashboard", async() => {
-        const response = await request(x.app).get("/facultydashboard");
+    test("It should respond with admin", async() => {
+        const response = await request(x.app).get("/admin");
         expect(response.statusCode).toBe(200);
     });
 
 });
-
-
-
 describe("GET / ", () =>{
-    test("It should respond with changepassword", async() => {
-        const response = await request(x.app).get("/changepassword");
+    test("It should respond with faculty", async() => {
+        const response = await request(x.app).get("/faculty");
         expect(response.statusCode).toBe(200);
     });
 
 });
-
 describe("GET / ", () =>{
-    test("It should respond with checkattendence", async() => {
-        const response = await request(x.app).get("/checkattendence");
-        expect(response.statusCode).toBe(200);
-    });
-
-});
-
-describe("GET / ", () =>{
-    test("It should respond with takeattendence", async() => {
-        const response = await request(x.app).get("/takeattendence");
-        expect(response.statusCode).toBe(200);
-    });
-
-});
-
-
-describe("GET / ", () =>{
-    test("It should respond with admindash", async() => {
-        const response = await request(x.app).get("/admindash");
+    test("It should respond with student", async() => {
+        const response = await request(x.app).get("/student");
         expect(response.statusCode).toBe(200);
     });
 
 });
 
 describe("GET / ", () =>{
-    test("It should respond with factosubject", async() => {
-        const response = await request(x.app).get("/factosubject");
+    test("It should respond with about", async() => {
+        const response = await request(x.app).get("/about");
         expect(response.statusCode).toBe(200);
     });
 
 });
 
 describe("GET / ", () =>{
-    test("It should respond with forgot", async() => {
-        const response = await request(x.app).get("/forgot");
+    test("It should respond with less", async() => {
+        const response = await request(x.app).get("/less");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+describe("GET / ", () =>{
+    test("It should respond with faq", async() => {
+        const response = await request(x.app).get("/faq");
         expect(response.statusCode).toBe(200);
     });
 
 });
 
 describe("GET / ", () =>{
-    test("It should respond with logout", async() => {
-        const response = await request(x.app).get("/logout");
+    test("It should respond with", async() => {
+        const response = await request(x.app).get("/");
         expect(response.statusCode).toBe(200);
     });
 
 });
 
 describe("GET / ", () =>{
-    test("It should respond with reset", async() => {
-        const response = await request(x.app).get("/reset");
+    test("It should respond with usename", async() => {
+        const response = await request(x.app).get("/getpass_student:username");
         expect(response.statusCode).toBe(200);
     });
 
 });
 
 describe("GET / ", () =>{
-    test("It should respond with newfac", async() => {
-        const response = await request(x.app).get("/newfac");
+    test("It should respond with username", async() => {
+        const response = await request(x.app).get("/startsession:username");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with getall", async() => {
+        const response = await request(x.app).get("/getall");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with getall_fac", async() => {
+        const response = await request(x.app).get("/getall_fac");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with search", async() => {
+        const response = await request(x.app).get("/getall_attend:search");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with check:search", async() => {
+        const response = await request(x.app).get("/getall_check:search");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with student:username", async() => {
+        const response = await request(x.app).get("/getpass_student:username");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with changepass", async() => {
+        const response = await request(x.app).get("/getchangepass:password");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with search name", async() => {
+        const response = await request(x.app).get("/search/:name");
+        expect(response.statusCode).toBe(200);
+    });
+
+});
+
+describe("GET / ", () =>{
+    test("It should respond with fac:name", async() => {
+        const response = await request(x.app).get("/search_fac/:name");
         expect(response.statusCode).toBe(200);
     });
 
